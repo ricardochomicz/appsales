d7dd1ccd5e552cb1c141d1b1ddca5671306e1928
Ricardo <ricardo.chomicz@gmail.com>
2021-12-21 20:55:48 -0300

    Cors Middleware

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->is('api/*')) {
+            header('Access-Control-Allow-Origin: http://localhost:4200');
+            header('Access-Control-Allow-Headers: Content-Type');
+            // header('');
+        }
+        return $next($request);
+    }
+}
